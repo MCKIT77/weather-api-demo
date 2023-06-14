ad054c04dca0bd491c454ca64d2ef879eab7df3b
Jacob McKitrick <jakemckit@gmail.com>
2023-06-13 20:06:59 -0400

    fixed bugs with showing previous searches, and added icon functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,13 +9,12 @@ geocoder
 
 http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 */
-
 $(document).ready(function () {
     var apiKey = '1fb6ecc7a506e4aafe86c237729982b0';
     var baseUrl = 'https://api.openweathermap.org/data/2.5/forecast?';
-    var endOfUrl = '&appid=' + apiKey + '&units=imperial';
+    var endOfUrl = `&appid=${apiKey}&units=imperial`;
     var geoBaseUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=';
-    var geoEndOfUrl = '&limit=1&appid=' + apiKey;
+    var geoEndOfUrl = `&limit=1&appid=${apiKey}`;
     var mainUrl = '';
     var searchBtnEl = $('#search-btn');
     var searchBoxEl = $('#search-box');
@@ -28,10 +27,16 @@ $(document).ready(function () {
         var cityName = searchBoxEl.val().trim();
         var cityPreview = cityName.split(',');
         var cityDisplay = cityPreview[0];
-        var userInput = cityName.toLowerCase() + ',us';
-        var reqUrl = geoBaseUrl + userInput + geoEndOfUrl;
+        var userInput = `${cityName.toLowerCase()},us`;
+        var reqUrl = `${geoBaseUrl}${userInput}${geoEndOfUrl}`;
         getCityName(reqUrl);
+        // clearUserInput();
+
     }
+
+    // function clearUserInput() {
+    //     $('#search-box').val('');
+    // }
 
     function getCityName(reqUrl) {
         fetch(reqUrl)
@@ -41,7 +46,7 @@ $(document).ready(function () {
             .then(function (response) {
                 var cityLat = response[0].lat;
                 var cityLon = response[0].lon;
-                mainUrl = baseUrl + 'lat=' + cityLat + '&lon=' + cityLon + endOfUrl;
+                mainUrl = `${baseUrl}lat=${cityLat}&lon=${cityLon}${endOfUrl}`;
                 getWeather(mainUrl);
             });
     }
@@ -69,9 +74,9 @@ $(document).ready(function () {
         var currentHumidity = currentWeather.main.humidity;
 
         var currentWeatherEl = $('<div>').addClass('current-weather');
-        currentWeatherEl.append($('<p>').text('Temperature: ' + currentTemp + ' °F'));
-        currentWeatherEl.append($('<p>').text('Wind Speed: ' + currentWind + ' mph'));
-        currentWeatherEl.append($('<p>').text('Humidity: ' + currentHumidity + '%'));
+        currentWeatherEl.append($('<p>').text(`Temperature: ${currentTemp} °F`));
+        currentWeatherEl.append($('<p>').text(`Wind Speed: ${currentWind} mph`));
+        currentWeatherEl.append($('<p>').text(`Humidity: ${currentHumidity}%`));
         contentEl.append(currentWeatherEl);
 
         // Display 5-day forecast
@@ -89,12 +94,15 @@ $(document).ready(function () {
             var forecastTemp = forecast.main.temp;
             var forecastWind = forecast.wind.speed;
             var forecastHumidity = forecast.main.humidity;
+            var weatherIcon = $('<img>').attr('src', `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`)
 
             var forecastCard = $('<div>').addClass('card dark-grey');
-            forecastCard.append($('<h4>').text(forecastDay + ', ' + forecastNumericDate));
-            forecastCard.append($('<p>').text('Temperature: ' + forecastTemp + ' °F'));
-            forecastCard.append($('<p>').text('Wind Speed: ' + forecastWind + ' mph'));
-            forecastCard.append($('<p>').text('Humidity: ' + forecastHumidity + '%'));
+
+            forecastCard.append($('<h4>').text(`${forecastDay}, ${forecastNumericDate}`));
+            forecastCard.append(weatherIcon);
+            forecastCard.append($('<p>').text(`Temperature: ${forecastTemp} °F`));
+            forecastCard.append($('<p>').text(`Wind Speed: ${forecastWind} mph`));
+            forecastCard.append($('<p>').text(`Humidity: ${forecastHumidity}%`));
 
             forecastWeatherEl.append(forecastCard);
         });
@@ -110,7 +118,7 @@ $(document).ready(function () {
             var buttonEl = $('<button>').addClass('search-button').text(search.displayName);
             previousSearchesEl.append(buttonEl);
         });
-
+        $('.previous-searches').text('');
         searchAreaEl.append(previousSearchesEl);
     }
 
@@ -120,14 +128,26 @@ $(document).ready(function () {
     }
 
     function saveSearch(search) {
-        var previousSearches = getPreviousSearches();
         var displayName = search.split(',')[0];
         var searchObject = {
             search: search,
             displayName: displayName
         };
 
-        previousSearches.push(searchObject);
+        var previousSearches = getPreviousSearches();
+        var existingSearchIndex = previousSearches.findIndex(function (prevSearch) {
+            return prevSearch.search === search;
+        });
+
+        if (existingSearchIndex !== -1) {
+            previousSearches.splice(existingSearchIndex, 1);
+        }
+
+        previousSearches.unshift(searchObject);
+        if (previousSearches.length > 5) {
+            previousSearches.pop();
+        }
+
         localStorage.setItem('previousSearches', JSON.stringify(previousSearches));
     }
 
@@ -144,156 +164,21 @@ $(document).ready(function () {
             });
     }
 
+    $('#clear-prev-searches').on('click', function (event) {
+
+        event.stopPropagation();
+
+        clearSearchHistory();
+        displayPreviousSearches();
+    });
+
+    function clearSearchHistory() {
+        localStorage.removeItem('previousSearches');
+        $('.previous-searches').empty(); // Clear the previous search buttons
+        displayPreviousSearches();
+
+    }
+
     searchBtnEl.on('click', buildGeoReq);
+    displayPreviousSearches();
 });
-
-
-
-
-
-
-
-
-    // var apiKey = '1fb6ecc7a506e4aafe86c237729982b0';
-
-    // var baseUrl = `http://api.openweathermap.org/data/2.5/forecast?`;
-
-
-
-    // var endOfUrl = `&appid=${apiKey}&units=imperial`;
-
-
-
-    // var geoBaseUrl = `http://api.openweathermap.org/geo/1.0/direct?q=`;
-
-    // var geoEndOfUrl = `&limit=1&appid=${apiKey}`;
-
-    // var mainUrl = '';
-
-    // var searchBtnEl = $('#search-btn');
-
-    // var searchBoxEl = $('#search-box');
-
-    // var contentEl = $('#content-side');
-
-
-
-
-
-
-    // function buildGeoReq(event) {
-    //     event.preventDefault();
-    //     event.stopPropagation();
-
-    //     var cityName = searchBoxEl.val().trim();
-
-    //     var cityPreview = cityName.split(',');
-    //     var cityDisplay = cityPreview[0];
-
-
-
-    //     var userInput = `${cityName.toLowerCase()},us`;
-
-    //     console.log(cityName.toString());
-
-    //     var reqUrl = `${geoBaseUrl}${userInput}${geoEndOfUrl}`;
-    //     console.log(reqUrl);
-    //     getCityName(reqUrl);
-
-
-
-
-
-
-
-
-
-    // }
-
-
-
-
-
-
-
-
-
-    // /*, {
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 'Authorization': `Bearer ${apiKey}`
-    //             }
-    //         }
-    // */
-
-
-    // function getCityName(reqUrl) {
-    //     fetch(reqUrl)
-    //         .then(function (response) {
-    //             return response.json();
-    //         })
-    //         .then(function (response) {
-    //             //getting lat and lon
-    //             var cityLat = response[0].lat;
-    //             var cityLon = response[0].lon;
-    //             mainUrl = '';
-    //             //building url and returning
-    //             mainUrl = `${baseUrl}lat=${cityLat}&lon=${cityLon}${endOfUrl}`;
-    //             console.log(mainUrl);
-    //             getWeather(mainUrl);
-
-    //         })
-
-
-
-    // }
-
-
-    // //function to display content
-    // function displayData(data) {
-    //     contentEl.textContent = ''
-
-    //     //must create many variables from data
-
-
-    //     var weather = list[current];
-    //     var list = data.list;
-
-
-
-    //     var cityName = searchBoxEl.val().trim();
-
-    //     var cityPreview = cityName.split(',');
-    //     var cityDisplay = cityPreview[0];
-
-    //     var cityDisplayEl = document.createElement('h2');
-    //     cityDisplayEl.textContent = cityDisplay;
-
-    //     console.log(list);
-    //     // list.forEach(function (listItem) {
-
-
-    //     // })
-
-    //     // need to display temp, wind, and humidity
-    //     console.log(data);
-    // }
-
-
-
-    // function getWeather(mainUrl) {
-    //     fetch(mainUrl)
-    //         .then(function (response) {
-    //             return response.json();
-    //         })
-    //         .then(function (data) {
-    //             displayData(data);
-    //         })
-
-    // }
-
-
-
-
-    // searchBtnEl.on('click', buildGeoReq);
-// })
