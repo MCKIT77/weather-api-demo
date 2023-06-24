643027e1a0f79fe92ffbd35dfa202ac121d8ab0f
Jacob McKitrick <jakemckit@gmail.com>
2023-06-24 18:23:09 -0400

    fixed alot of bugs, still working on it

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,7 +20,32 @@ $(document).ready(function () {
     var searchBoxEl = $('#search-box');
     var contentEl = $('#content-side');
     var searchAreaEl = $('.search-area');
+    var prevCityName = '';
 
+    displayPreviousSearches();
+
+
+    function buildPrevGeoReq(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        clearContent();
+        prevCityName = '';
+        var cityName = event.target.textContent;
+        prevCityName = cityName;
+        var cityPreview = cityName.split(',');
+        var cityDisplay = cityPreview[0];
+        var userInput = `${cityName.toLowerCase()},us`;
+        var reqUrl = `${geoBaseUrl}${userInput}${geoEndOfUrl}`;
+
+        getPrevCityName(reqUrl);
+
+        // clearUserInput();
+    }
+
+    function clearContent() {
+        contentEl.empty();
+
+    }
     function buildGeoReq(event) {
         event.preventDefault();
         event.stopPropagation();
@@ -32,8 +57,22 @@ $(document).ready(function () {
         getCityName(reqUrl);
         // clearUserInput();
 
+
     }
 
+
+    function getPrevCityName(reqUrl) {
+        fetch(reqUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (response) {
+                var cityLat = response[0].lat;
+                var cityLon = response[0].lon;
+                mainUrl = `${baseUrl}lat=${cityLat}&lon=${cityLon}${endOfUrl}`;
+                getPrevWeather(mainUrl);
+            });
+    }
     // function clearUserInput() {
     //     $('#search-box').val('');
     // }
@@ -51,10 +90,10 @@ $(document).ready(function () {
             });
     }
 
-    function displayData(data) {
+    function displayPrevData(data) {
         contentEl.empty(); // Clear the content
 
-        var cityName = searchBoxEl.val().trim();
+        var cityName = prevCityName;
         var cityPreview = cityName.split(',');
         var cityDisplay = cityPreview[0];
 
@@ -68,19 +107,21 @@ $(document).ready(function () {
         contentEl.append(cityDisplayEl, currentDateEl);
 
 
-        // Save search in local storage
-        saveSearch(cityName);
 
-        // Display previous searches
-        displayPreviousSearches();
+
+
+
 
         // Display current weather
         var currentWeather = data.list[0];
         var currentTemp = currentWeather.main.temp;
         var currentWind = currentWeather.wind.speed;
         var currentHumidity = currentWeather.main.humidity;
+        var currentIcon = $('<img>').attr('src', `https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`);
+        console.log(currentWeather);
 
         var currentWeatherEl = $('<div>').addClass('current-weather');
+        currentWeatherEl.append(currentIcon);
         currentWeatherEl.append($('<p>').text(`Temperature: ${currentTemp} °F`));
         currentWeatherEl.append($('<p>').text(`Wind Speed: ${currentWind} mph`));
         currentWeatherEl.append($('<p>').text(`Humidity: ${currentHumidity}%`));
@@ -117,17 +158,91 @@ $(document).ready(function () {
         contentEl.append(forecastWeatherEl);
     }
 
+    function displayData(data) {
+        contentEl.empty(); // Clear the content
+
+        var cityName = searchBoxEl.val().trim();
+        var cityPreview = cityName.split(',');
+        var cityDisplay = cityPreview[0];
+
+        var cityDisplayEl = $('<h2>').text(cityDisplay.toUpperCase());
+        var currentDate = new Date().toLocaleDateString('en-US', {
+            weekday: 'short',
+            month: 'short',
+            day: 'numeric'
+        });
+        var currentDateEl = $('<h2>').text(currentDate);
+        contentEl.append(cityDisplayEl, currentDateEl);
+
+
+        // Save search in local storage
+        saveSearch(cityName);
+
+        // Display previous searches
+        displayPreviousSearches();
+
+        // Display current weather
+        var currentWeather = data.list[0];
+        var currentTemp = currentWeather.main.temp;
+        var currentWind = currentWeather.wind.speed;
+        var currentHumidity = currentWeather.main.humidity;
+        var currentIcon = $('<img>').attr('src', `https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png`);
+        console.log(currentWeather);
+
+        var currentWeatherEl = $('<div>').addClass('current-weather');
+        currentWeatherEl.append(currentIcon);
+        currentWeatherEl.append($('<p>').text(`Temperature: ${currentTemp} °F`));
+        currentWeatherEl.append($('<p>').text(`Wind Speed: ${currentWind} mph`));
+        currentWeatherEl.append($('<p>').text(`Humidity: ${currentHumidity}%`));
+        contentEl.append(currentWeatherEl);
+
+        // Display 5-day forecast
+        var forecastWeather = data.list.slice(1, 6); // Get the next 5 days' data
+        var forecastWeatherEl = $('<div>').addClass('forecast-weather');
+
+        var currentDate = new Date(); // Get the current date
+
+        forecastWeather.forEach(function (forecast, index) {
+            var forecastDate = new Date();
+            forecastDate.setDate(currentDate.getDate() + index + 1); // Increase the date by 1 day for each forecast card
+
+            var forecastDay = forecastDate.toLocaleDateString('en-US', { weekday: 'short' });
+            var forecastNumericDate = forecastDate.getDate();
+            var forecastTemp = forecast.main.temp;
+            var forecastWind = forecast.wind.speed;
+            var forecastHumidity = forecast.main.humidity;
+            var weatherIcon = $('<img>').attr('src', `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`)
+
+            var forecastCard = $('<div>').addClass('card dark-grey');
+
+            forecastCard.append($('<h4>').text(`${forecastDay}, ${forecastNumericDate}`));
+            forecastCard.append(weatherIcon);
+            forecastCard.append($('<p>').text(`Temperature: ${forecastTemp} °F`));
+            forecastCard.append($('<p>').text(`Wind Speed: ${forecastWind} mph`));
+            forecastCard.append($('<p>').text(`Humidity: ${forecastHumidity}%`));
+
+            forecastWeatherEl.append(forecastCard);
+        });
+
+        contentEl.append(forecastWeatherEl);
+        $('#search-box').val('');
+    }
+
     function displayPreviousSearches() {
         var previousSearches = getPreviousSearches();
 
         var previousSearchesEl = $('<div>').addClass('previous-searches');
         previousSearches.forEach(function (search) {
+
             var buttonEl = $('<button>').addClass('search-button').text(search.displayName);
+            buttonEl.addClass('prev-search-btn');
             previousSearchesEl.append(buttonEl);
         });
         $('.previous-searches').text('');
         searchAreaEl.append(previousSearchesEl);
     }
+
+
 
     function getPreviousSearches() {
         var searches = localStorage.getItem('previousSearches');
@@ -170,11 +285,23 @@ $(document).ready(function () {
                 console.log('Error:', error);
             });
     }
+    function getPrevWeather(url) {
+        fetch(url)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                displayPrevData(data);
+            })
+            .catch(function (error) {
+                console.log('Error:', error);
+            });
+    }
 
     $('#clear-prev-searches').on('click', function (event) {
-
+        event.preventDefault();
         event.stopPropagation();
-
+        $(".previous-searches").empty()
         clearSearchHistory();
         displayPreviousSearches();
     });
@@ -186,6 +313,7 @@ $(document).ready(function () {
 
     }
 
+
     searchBtnEl.on('click', buildGeoReq);
-    displayPreviousSearches();
+    $('.prev-search-btn').on('click', buildPrevGeoReq);
 });
